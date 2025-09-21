f243aa696a08ce2b454a073d350ba0316467443b
Dhruv kumar <dhruvkumar64419@gmail.com>
2025-09-21 12:49:04 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,20 @@
+#Task 1
+
+a=input("Enter the firt number: ")
+b=input("Enter the second number: ")
+a=int(a)
+b=int(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print('\nAddition: ', c)
+print('Subtraction: ', d)
+print('Multiplication: ', e)
+print('Division: ', f)
+
+#Task 2
+
+a=input('Enter your first name: ')
+b=input('Enter your last name: ')
+print('\nHello',a,b+'!','welcome to the Python program.')
