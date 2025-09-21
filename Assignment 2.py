baf3fe4dc94a9dc638c8ff8539c9becd07c8cd20
Dhruv kumar <dhruvkumar64419@gmail.com>
2025-09-21 13:07:44 +0530

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,15 @@
+# Task 1
+
+a=input("Enter a number: ")
+a=int(a)
+if (a)%2==0:
+    print(a,'is an even number')
+else:
+    print(a,'is an Odd number')
+
+# Task 2
+
+total=0
+for i in range(1,51):
+    total=total+i
+print('The sum of numbers from 1 to 50 is:',total)
